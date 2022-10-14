8641a451ce4143bbd13a52b5c5fdc7c79b11d21f
hyoozo <eo_8282@naver.com>
2022-10-14 09:06:09 +0900

    Initial commit

---
+++ src/firstExam/Example.java
@@ -0,0 +1,12 @@
+package firstExam;
+//한줄 주석문
+public class Example {
+    //main()메소드에서 실행 시작
+    public static void main(String[] args) {
+        System.out.println("이름 : 이현주");
+        System.out.println("주소 : 서울시 용산구");
+        System.out.print("나의 각오 : ");
+        System.out.println("멋쟁이 개발자 되기");
+
+    }
+}
---
+++ src/firstExam/HelloWorld.java
@@ -0,0 +1,9 @@
+package firstExam;
+//한줄 주석문
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("힘들지만 빨리 적응할 수 있도록");
+        System.out.println("오늘도 화이팅");
+    }
+}
